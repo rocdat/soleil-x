49f023e268b60d86902ee52ede12e28f0237352a
Alan Heirich <aheirich@stanford.edu>
2017-07-03 17:09:20 -0700

    add libGLU to multinode runs

--- scripts/run_multinode.bash
+++ scripts/run_multinode.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-LD_LIBRARY_PATH=.:$LEGION_PATH/bindings/terra/ mpirun -H n0000,n0001,n0002,n0003 -x LD_LIBRARY_PATH ./soleil.exec -ll:cpu 8 -ll:util 1 -ll:dma 1 -ll:csize 30000
+LD_LIBRARY_PATH=.:$LEGION_PATH/bindings/terra/:/usr/lib/x86_64-linux-gnu/ mpirun -H n0000,n0001,n0002,n0003 -x LD_LIBRARY_PATH ./soleil.exec -ll:cpu 8 -ll:util 1 -ll:dma 1 -ll:csize 30000
 

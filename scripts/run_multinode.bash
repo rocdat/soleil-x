0dd9e222249c741ec1fd8b77c94845cd6ba39867
Alan Heirich <aheirich@stanford.edu>
2017-07-04 21:24:35 -0700

    scripting

--- scripts/run_multinode.bash
+++ scripts/run_multinode.bash
@@ -1,2 +1,6 @@
 #!/bin/bash
+rm -f ~/soleil.exec ~/librender.so ~/libsoleil_mapper.so
+ln -s $SOLEIL_PATH/src/soleil.exec ~
+ln -s $SOLEIL_PATH/src/librender.so ~
+
 GASNET_BACKTRACE=1 LD_LIBRARY_PATH=.:$LEGION_PATH/bindings/terra/:/usr/lib/x86_64-linux-gnu/ mpirun -H n0000,n0001,n0002,n0003 -x LD_LIBRARY_PATH -x GASNET_BACKTRACE ./soleil.exec -ll:cpu 8 -ll:util 1 -ll:dma 1 -ll:csize 30000

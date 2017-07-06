2351d7e1b281700874e98a6c38ec5e624da736f2
Alan Heirich <aheirich@stanford.edu>
2017-07-06 05:19:18 +0200

    scripting
    not happy with install_piz_daint.bash because it exits with error due to terra.build

---
+++ scripts/install_piz_daint.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+module unload PrgEnv-cray
+module load PrgEnv-gnu
+module load daint-mc
+unset LG_RT_DIR
+
+cd ${LEGION_PATH}/language
+CC=cc CXX=CC HOST_CC=gcc HOST_CXX=g++ scripts/setup_env.py || echo continuing
+echo stopped here because of error, continue by hande
+mv terra.build terra.build.master
+git clone -b luajit2.1 https://github.com/magnatelee/terra.git terra.build
+cd terra.build
+CC=gcc CXX=g++ make LLVM_CONFIG=`readlink -f ../llvm/install/bin/llvm-config` CLANG=`readlink -f ../llvm/install/bin/clang` -j
+cd ..
+
--- scripts/prepare.bash
+++ scripts/prepare.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-source $SOLEIL_PATH/scripts/setup.bash
 git clone -b master https://github.com/StanfordLegion/legion.git $LEGION_PATH
 git clone https://github.com/manopapad/liszt-legion.git $LISZT_PATH
 git clone https://github.com/stanfordhpccenter/soleil-x.git $SOLEIL_PATH
@@ -10,5 +9,20 @@ if [[ "`hostname`" == "sapling" ]]
 then
   CONDUIT=ibv ./install.py --gasnet
 else
-  ./install.py
+  HOSTNAME=`hostname`
+  MYHOST=`echo ${HOSTNAME} | sed -e 's/daint.*/daint/'`
+  if [[ "${MYHOST}" == "daint" ]]
+  then
+    ${SOLEIL_PATH}/scripts/install_piz_daint.bash
+    mv terra.build terra.build.master
+    CC=cc CXX=CC HOST_CC=gcc HOST_CXX=g++ scripts/setup_env.py
+    git clone -b luajit2.1 https://github.com/magnatelee/terra.git terra.build
+
+    cd terra.build
+    CC=gcc CXX=g++ make LLVM_CONFIG=`readlink -f ../llvm/install/bin/llvm-config` CLANG=`readlink -f ../llvm/install/bin/clang` -j
+    cd ..
+
+  else
+    ./install.py
+  fi
 fi

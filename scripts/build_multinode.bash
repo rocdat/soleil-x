5545d708f254304dae37b1a9f0d8815df1f27c48
Alan Heirich <aheirich@stanford.edu>
2017-07-17 20:30:52 -0700

    ibuild script

--- scripts/build_multinode.bash
+++ scripts/build_multinode.bash
@@ -8,7 +8,7 @@ fi
 TESTCASE=$1
 if [[ "$1" == "" ]]
 then
-  cat ${SOLEIL_PATH}/testcases/taylor_with_smaller_particles/taylor_green_vortex_256_256_256.lua | sed -e "s:max_iter =.*:max_iter = 5,:" > testcase.lua
+  cat ${SOLEIL_PATH}/testcases/taylor_with_smaller_particles/taylor_green_vortex_256_256_256_modified.lua| sed -e "s:max_iter =.*:max_iter = 25,:" > testcase.lua
   TESTCASE=testcase.lua
 fi
 
@@ -23,6 +23,7 @@ USE_HDF=0 DEBUG=0 SAVEOBJ=1 OBJNAME=soleil.exec \
 	-i ${TESTCASE} \
 	-fparallelize 1 \
 	-fparallelize-dop 2,2,1 \
+	-fflow-spmd 1 \
 #-fflow-spmd-shardsize 1
 
 echo start ${START}

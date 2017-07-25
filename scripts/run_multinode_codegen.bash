e91f9c8211378a2be3f495bc944f91d8e2592632
Alan Heirich <aheirich@stanford.edu>
2017-07-24 17:03:37 -0700

    iscripting

--- scripts/run_multinode_codegen.bash
+++ scripts/run_multinode_codegen.bash
@@ -18,13 +18,15 @@ then
   NUM_TREE_LEVELS=2
 fi
 
-GASNET_BACKTRACE=1 LD_LIBRARY_PATH=.:$LEGION_PATH/bindings/terra/:/usr/lib/x86_64-linux-gnu/:$SOLEIL_PATH/src/ \
+LEGION_BACKTRACE=1 LEGION_FREEZE_ON_ERROR=1 GASNET_BACKTRACE=1 LD_LIBRARY_PATH=.:$LEGION_PATH/bindings/terra/:/usr/lib/x86_64-linux-gnu/:$SOLEIL_PATH/src/ \
 	mpirun \
 		-bind-to none \
 		-npernode 1 \
 		-H n0000,n0001,n0002,n0003 \
 		-x LD_LIBRARY_PATH \
 		-x GASNET_BACKTRACE \
+		-x LEGION_BACKTRACE \
+		-x LEGION_FREEZE_ON_ERROR \
 	./soleil_${NUM_FRAGMENTS}_${NUM_TREE_LEVELS}.exec \
 		-ll:cpu 8 \
 		-ll:util 1 \

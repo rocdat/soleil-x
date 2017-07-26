45eba580da5401289f59a72d945621f680ccf6b0
Alan Heirich <aheirich@stanford.edu>
2017-07-26 20:33:25 +0200

    incremental

---
+++ src/bopdj.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+NODES=$1
+MESH=$2
+TESTCASE=$3
+NUM_FRAGMENTS=$4
+NUM_TREE_LEVELS=$5
+TIME_LIMIT=$6
+OUTDIR=$7
+
+if [[ "$TESTCASE" == "" ]]
+then
+  TESTCASE=taylor_green_vortex_512_512_256.lua
+fi
+
+cd $SOLEIL_PATH/src
+
+$SOLEIL_PATH/src/bmc.bash $NUM_FRAGMENTS $NUM_TREE_LEVELS $SOLEIL_PATH/testcases/taylor_with_smaller_particles/$TESTCASE $MESH
+
+JOBID=Job_$NODES
+mkdir -p ${OUTDIR}/${JOBID}
+EXEC=soleil_${NUM_FRAGMENTS}_${NUM_TREE_LEVELS}.exec
+mv ./${EXEC} ${OUTDIR}/${JOBID}
+mv ./*.so ${OUTDIR}/${JOBID}
+cat $SOLEIL_PATH/scripts/piz_daint_multinode_job.bash | \
+  sed -e "s/TIME_LIMIT/${TIME_LIMIT}/g" | \
+  sed -e "s/NODES/${NODES}/g" | \
+  sed -e "s/JOBID/${JOBID}/g" | \
+  sed -e "s/EXEC/${EXEC}/g" \
+  > ${OUTDIR}/${JOBID}/${JOBID}_piz_daint.bash
+

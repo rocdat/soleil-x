d8e751eeeb67a0d976fd05cda0b92f33e47dae2e
Alan Heirich <aheirich@stanford.edu>
2017-08-02 22:21:50 +0200

    use 2,2,1 for 4 nodes

--- scripts/build_weak_scaling_piz_daint_jobs.bash
+++ scripts/build_weak_scaling_piz_daint_jobs.bash
@@ -6,8 +6,8 @@ mkdir -p ${OUTDIR}
 # run with 2 cpus per node
 
 # nodes mesh testcase numfragments numtreelevels
-$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 4 4,4,2 taylor_green_vortex_512_512_256.lua 4 2 00:10:00 ${OUTDIR}
-$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 8 8,4,2 taylor_green_vortex_512_512_512.lua 8 3 00:10:00 ${OUTDIR}
+$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 4 2,2,1 taylor_green_vortex_512_512_256.lua 4 2 00:10:00 ${OUTDIR}
+$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 8 4,2,1 taylor_green_vortex_512_512_512.lua 8 3 00:10:00 ${OUTDIR}
 
 $SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 16 8,8,2 taylor_green_vortex_512_512_256.lua 16 4 00:05:00 ${OUTDIR}
 $SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 32 8,8,4 taylor_green_vortex_512_512_512.lua 32 5 00:05:00 ${OUTDIR}

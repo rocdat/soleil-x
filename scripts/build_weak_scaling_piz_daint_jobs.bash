fcfe11ae5a89d0f53e109e6ad83014be971f073e
Alan Heirich <aheirich@stanford.edu>
2017-08-04 00:48:11 +0200

    include full path to testcase

--- scripts/build_weak_scaling_piz_daint_jobs.bash
+++ scripts/build_weak_scaling_piz_daint_jobs.bash
@@ -6,16 +6,30 @@ mkdir -p ${OUTDIR}
 # run with 2 cpus per node
 
 # nodes mesh testcase numfragments numtreelevels
-$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 4 2,2,1 taylor_green_vortex_512_512_256.lua 4 2 00:10:00 ${OUTDIR}
-$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 8 4,2,1 taylor_green_vortex_512_512_512.lua 8 3 00:10:00 ${OUTDIR}
 
-$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 16 8,8,2 taylor_green_vortex_512_512_256.lua 16 4 00:05:00 ${OUTDIR}
-$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 32 8,8,4 taylor_green_vortex_512_512_512.lua 32 5 00:05:00 ${OUTDIR}
-$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 64 16,8,4 taylor_green_vortex_1024_512_512.lua 64 6 00:05:00 ${OUTDIR}
-$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 128 16,16,4 taylor_green_vortex_1024_1024_512.lua 128 7 00:05:00 ${OUTDIR}
-$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 256 16,16,8 taylor_green_vortex_1024_1024_1024.lua 256 8 00:05:00 ${OUTDIR}
-$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 512 32,16,8 taylor_green_vortex_2048_1024_1024.lua 512 9 00:05:00 ${OUTDIR}
-$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 1024 32,32,8 taylor_green_vortex_2048_2048_1024.lua 1024 10 00:05:00 ${OUTDIR}
-#$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 2048 16,16,8 taylor_green_vortex_2048_2048_2048.lua 2048 11 00:05:00 ${OUTDIR}
-#$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 4096 32,16,8 taylor_green_vortex_4096_2048_2048.lua 4096 12 00:05:00 ${OUTDIR}
-#$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 8192 32,32,8 taylor_green_vortex_4096_4096_2048.lua 8192 13 00:05:00 ${OUTDIR}
+# 4 nodes openmp
+$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 4 2,2,1 ../testcases/taylor_with_smaller_particles/taylor_green_vortex_512_512_256.lua 4 2 00:10:00 ${OUTDIR}
+
+# 8 nodes openmp
+$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 8 4,2,1 ../testcases/taylor_with_smaller_particles/taylor_green_vortex_512_512_512.lua 8 3 00:10:00 ${OUTDIR}
+
+# 16 nodes openmp
+$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 16 8,8,2 ../testcases/taylor_with_smaller_particles/taylor_green_vortex_512_512_256.lua 16 4 00:05:00 ${OUTDIR}
+
+# 32 nodes openmp
+$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 32 8,8,4 ../testcases/taylor_with_smaller_particles/taylor_green_vortex_512_512_512.lua 32 5 00:05:00 ${OUTDIR}
+
+# 64 nodes openmp
+$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 64 16,8,4 ../testcases/taylor_with_smaller_particles/taylor_green_vortex_1024_512_512.lua 64 6 00:05:00 ${OUTDIR}
+
+# 128 nodes openmp
+$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 128 16,16,4 ../testcases/taylor_with_smaller_particles/taylor_green_vortex_1024_1024_512.lua 128 7 00:05:00 ${OUTDIR}
+
+# 256 nodes openmp
+$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 256 16,16,8 ../testcases/taylor_with_smaller_particles/taylor_green_vortex_1024_1024_1024.lua 256 8 00:05:00 ${OUTDIR}
+
+# 512 nodes openmp
+$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 512 32,16,8 ../testcases/taylor_with_smaller_particles/taylor_green_vortex_2048_1024_1024.lua 512 9 00:05:00 ${OUTDIR}
+
+# 1024 nodes openmp
+$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 1024 32,32,8 ../testcases/taylor_with_smaller_particles/taylor_green_vortex_2048_2048_1024.lua 1024 10 00:05:00 ${OUTDIR}

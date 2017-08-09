f87db3868d6dc13559fdf154d12c8dfc398a2257
Alan Heirich <aheirich@stanford.edu>
2017-08-10 00:43:41 +0200

    reduce fragment count so compile time does not explode

--- scripts/batch_build_128.bash
+++ scripts/batch_build_128.bash
@@ -16,5 +16,5 @@ OUTDIR=$SOLEIL_PATH/src/piz_daint_jobs
 mkdir -p ${OUTDIR}
 cd src
 rm -rf piz_daint_jobs/Job_128
-$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 128 8,4,4 taylor_green_vortex_2048_1024_1024.lua 128 7 00:10:00 6 ${OUTDIR}
+$SOLEIL_PATH/scripts/build_one_piz_daint_job.bash 128 8,4,4 taylor_green_vortex_2048_1024_1024.lua 32 7 00:10:00 6 ${OUTDIR}
 

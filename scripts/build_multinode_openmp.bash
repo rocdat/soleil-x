339bb1b7379382e34dd5772e8846da5c53f21b8e
Alan Heirich <aheirich@stanford.edu>
2017-08-11 02:48:19 +0200

    replace NUM_FRAGMENTS in viz.rg

--- scripts/build_multinode_openmp.bash
+++ scripts/build_multinode_openmp.bash
@@ -52,6 +52,8 @@ git checkout -- $GEN
 source $SOLEIL_PATH/scripts/codegen.bash ${NUM_FRAGMENTS} ${NUM_TREE_LEVELS}
 for f in $GEN ; do cp tmp_src/$f $f ; done
 
+cat tmp_src/viz.rg | sed -e "s/NUM_FRAGMENTS/${NUM_FRAGMENTS}/" > viz.rg
+
 COMPILE_COMMAND=\
 "CC=gcc CXX=g++ USE_HDF=0 DEBUG=0 SAVEOBJ=1 OBJNAME=${EXEC} \
 	$LISZT_PATH/liszt-legion.sh $SOLEIL_PATH/src/soleil-x.t \

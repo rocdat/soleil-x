c67141a2cf44eb0f510601298aaab645ae61faba
Alan Heirich <aheirich@stanford.edu>
2017-08-24 17:55:53 +0200

    soleil_path

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -4,6 +4,6 @@ export LEGION_PATH=`pwd`/legion
 export LG_RT_PATH=$LEGION_PATH/runtime
 export LISZT_PATH=`pwd`/liszt-legion
 export OSMESA_PATH=`pwd`/osmesa
-export SOLEIL_PATH=`pwd`/soleil-x
+export SOLEIL_PATH=`pwd`/soleil-m
 export LG_RT_PATH=$LEGION_PATH/runtime
 export LG_RT_DIR=$LG_RT_PATH
